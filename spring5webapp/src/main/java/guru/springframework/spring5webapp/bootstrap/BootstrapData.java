5d7c6ea9acb1044d259ca5693162504b25d1b6dd
Maria <formacionjee@gmail.com>
2020-05-19 19:25:41 +0200

    Add up to 19: H2 Database Console

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.models.Author;
 import guru.springframework.spring5webapp.models.Book;
+import guru.springframework.spring5webapp.models.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,25 +14,46 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println("Started Bootstrap");
+
+        Publisher Manning = new Publisher();
+        Manning.setName("Manning");
+        Manning.setAddressLine1("USA 3");
+        Manning.setState("Iowa");
+        publisherRepository.save(Manning);
+        System.out.println("Number of publishers: " + publisherRepository.count());
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123456");
+
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(Manning);
+        Manning.getBooks().add(ddd);
+
+
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(Manning);
 
-        System.out.println("Started Bootstrap");
+
+
+
         System.out.println("Number of Books:" + bookRepository.count());
+        System.out.println("Publisher number of books: "+ Manning.getBooks().size());
 
     }
 }
--- spring5webapp/src/main/java/guru/springframework/spring5webapp/models/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/models/Book.java
@@ -12,6 +12,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(
             name = "author_book",
@@ -57,6 +60,14 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
